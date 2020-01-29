090c87b6da76de4ca42fe39ca0f058a975a6cfb3
lucasfstos95 <lucas.fstos95@gmail.com>
2020-01-29 19:58:04 -0300

    up Generate html examples

---
+++ Exercicios IAPCP/exercicio_9_30.py
@@ -0,0 +1,24 @@
+#Exercicio 9.30 - Geração de uma pag html a partir de um dicionario
+filmes = {
+    'drama': ['Cidadão Kane', 'O poderoso Chefão'],
+    'comédia': ['Tempos Modernos', 'American Pie', 'Dr. Dolittle'],
+    'policial': ['Chuva Negra', 'Desejo de Matar', 'Difícil de Matar'],
+    'guerra': ['Rambo', 'Platoon', 'Tora!Tora!Tora!']
+}
+
+with open("arquivos/filmes.html", "w", encoding="utf-8") as pagina:
+    pagina.write("""
+<!DOCTYPE html>
+<html lang="pt-br>
+<head>
+<meta charset="utf-8">
+<title>Filmes</title>
+</head>
+<body>
+    """)
+    for c, v in filmes.items():
+        pagina.write(f"<ul><h1>{c}</h1>\n")
+        for e in v:
+            pagina.write(f"<li>{e}</li>\n")
+        pagina.write(f"</ul>")
+    pagina.write("</body></html>")
---
+++ Exercicios IAPCP/programa_9_8.py
@@ -0,0 +1,23 @@
+#Programa 9.8 - Geração de uma pag html a partir de um dicionario
+filmes = {
+    'drama': ['Cidadão Kane', 'O poderoso Chefão'],
+    'comédia': ['Tempos Modernos', 'American Pie', 'Dr. Dolittle'],
+    'policial': ['Chuva Negra', 'Desejo de Matar', 'Difícil de Matar'],
+    'guerra': ['Rambo', 'Platoon', 'Tora!Tora!Tora!']
+}
+
+with open("arquivos/filmes.html", "w", encoding="utf-8") as pagina:
+    pagina.write("""
+<!DOCTYPE html>
+<html lang="pt-br>
+<head>
+<meta charset="utf-8">
+<title>Filmes</title>
+</head>
+<body>
+    """)
+    for c, v in filmes.items():
+        pagina.write(f"<h1>{c}</h1>\n")
+        for e in v:
+            pagina.write(f"<h2>{e}</h2>\n")
+    pagina.write("</body></html>")
